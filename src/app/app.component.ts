fcbe3323dd8aa0bce2fa9a7c29604ca287c6e921
GottfriedOswald <go.oswald@t-online.de>
2022-01-26 23:36:35 +0100

    add images and changing screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'gottfried_oswald.de';
+export class AppComponent implements OnInit {
+  // title = 'gottfried_oswald.de';
+
+  images = ['laptop002.jpg', 'code-on-screen.jpg', 'college001.jpg'];
+  currentImage = 0;
+  ngOnInit() {
+      this.updateImage();
+  }
+
+  updateImage(){
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+    },8000);
+  }
 }
