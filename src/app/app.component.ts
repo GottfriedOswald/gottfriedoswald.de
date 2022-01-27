0f6c15bf6793577b607738c37917222942814ffe
GottfriedOswald <go.oswald@t-online.de>
2022-01-27 10:24:19 +0100

    center Images on Screen, set looping  Animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  // title = 'gottfried_oswald.de';
 
   images = ['laptop002.jpg', 'code-on-screen.jpg', 'college001.jpg'];
   currentImage = 0;
+  showImage = true;
+
+  // ngOnInit(){} beinhaltet Funktionen die beim Start ausgeführt werden.
+  // OnInit muss aber bei der Klassenerstellung implementiert werden
   ngOnInit() {
       this.updateImage();
   }
 
+  // Zähler wird hochgezählt und mit der Arraylänge verglichen
+  // wenn Zähler denselben Wert hat wie die Arraylänge wird durch
+  // Restwertberechnung der Zähler wieder auf 0 gesetzt
   updateImage(){
     setInterval(() => {
       this.currentImage++;
       this.currentImage = this.currentImage % this.images.length;
+      this.hideImage();
     },8000);
+  }
+
+  // blendet für kurze Zeit das Bild aus um das unschöne Flackern
+  // beim Zurücksetzen des Zooms auszublenden
+  hideImage(){
+    this.showImage = false;
+    setTimeout(()=>{
+      this.showImage = true;
+    }, 200);
   }
 }
